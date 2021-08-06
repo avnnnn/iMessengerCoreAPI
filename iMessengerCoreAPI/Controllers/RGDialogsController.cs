c4f1a4fd23bea0d9e986763d57985dbde377bb08
avnnnn <48981655+avnnnn@users.noreply.github.com>
2021-08-06 15:55:24 +0600

    Добавьте файлы проекта.

---
+++ iMessengerCoreAPI/Controllers/RGDialogsController.cs
@@ -0,0 +1,28 @@
+﻿using iMessengerCoreAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using iMessengerCoreAPI.Utils;
+namespace iMessengerCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RGDialogsController : ControllerBase
+    {
+        [HttpPost]
+        public JsonResult getDialog(List<Guid> clients)
+        {
+            List<RGDialogsClients> dialogs = new RGDialogsClients().Init();
+            List<RGDialogsClients> dialogsCLients = new List<RGDialogsClients>();
+            foreach (var client in clients)
+            {
+                dialogsCLients.Add(dialogs.FirstOrDefault(x => Guid.Equals(x.IDClient, client)));
+            }
+            return new JsonResult(RGDialogsUtil.FindDialog(dialogsCLients));
+        }
+     
+        
+    }
+}
---
+++ iMessengerCoreAPI/Utils/RGDialogsUtil.cs
@@ -0,0 +1,32 @@
+﻿
+using iMessengerCoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace iMessengerCoreAPI.Utils
+{
+    public class RGDialogsUtil
+    {
+        public static Guid FindDialog(List<RGDialogsClients> dialogCLinets)
+        {
+            Guid dialogId;
+            if (!dialogCLinets.Contains(null))
+            {
+                dialogId = dialogCLinets[0].IDRGDialog;
+            }
+            else
+            {
+                return Guid.NewGuid();
+            }
+            foreach (var dialog in dialogCLinets)
+            {
+                if (!Guid.Equals(dialog.IDRGDialog, dialogId))
+                {
+                    return Guid.NewGuid();
+                }
+            }
+            return dialogId;
+        }
+    }
+}
